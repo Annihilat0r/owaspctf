757dfaa6e8d689b2b5c4662d33c8cdad2fe318b4
q <q>
2019-10-04 10:49:42 +0300

    001 file hashes logic

--- 001lfihashes/src/developers_mod_v7499.php
+++ 001lfihashes/src/developers_mod_v7499.php
@@ -1,8 +1,7 @@
 <?php
 echo("<h3>Short FileName read</h3>");
-$filename = pathinfo($_REQUEST["file"])["filename"].".php";
-
-if($_REQUEST["file"]){
+$filename = explode(".",$_REQUEST["file"])[0].".php";
+if($_REQUEST["file"] && (pathinfo($_REQUEST["file"])['extension']) == "php"){
 		if(substr(md5($_REQUEST["file"]),0,8) == substr(md5("developers_mod_v7499.php"),0,8)) {
 			echo("<pre>".htmlentities(file_get_contents($filename))."</pre>");
 	}
--- 007roulette/src/index.php
+++ 007roulette/src/index.php
@@ -1,13 +1,13 @@
 <?php
 
 echo('<center><form method="post">
-<input type="text" name="flag_ckeck"><br>
+<input type="text" name="flag_check"><br>
 <input type="submit" value="Enter">');
 echo('</form>');
 $flag="FLAG{To_infinity...and_beyond!}";
 $answer="";
-$check_string=md5($_REQUEST['flag_ckeck']);
-if(!empty($_REQUEST['flag_ckeck'])){
+$check_string=md5($_REQUEST['flag_check']);
+if(!empty($_REQUEST['flag_check'])){
 	for($i = 0; $i <= 32; $i++){
 
 		if($check_string[$i]==5){
