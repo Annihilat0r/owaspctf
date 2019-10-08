d43e93965a6735bece6f3fead6e8583eae7ff814
q <q>
2019-10-08 13:42:31 +0300

    add // to jquery

--- 006hashcollision/src/index.php
+++ 006hashcollision/src/index.php
@@ -32,7 +32,7 @@
 	</ul>
 </div>
 <!-- partial -->
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script  src="./script.js"></script>
 
 </body>
